3615ce8a9873ec74474b357b1d6b30b4997cc2b7
fawaz-ansari123 <58271286+fawaz-ansari123@users.noreply.github.com>
2019-11-29 04:31:42 +0500

    Add files via upload

---
+++ frontpage/font/src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import Shome from './shome.js';
+
+
+
+class App extends Component{
+  render(){
+    return(
+    <div>
+    <Shome/>
+    </div>
+    )
+  }
+}
+
+export default App;
---
+++ frontpage/font/src/shome.js
@@ -0,0 +1,32 @@
+import React,{Component} from 'react';
+import yes from './yes.css'
+
+
+class Shome extends Component{
+  render(){
+      
+    return(
+        
+		<div class="container">
+        <div class="tatti">
+        <h1>Welcome to SOFFICE</h1>
+        <p class="in"> "A single stop solution for all problems"</p>
+        </div>
+        
+        
+<div class="w3-sand w3-grayscale w3-large">
+
+
+<div class="w3-container" id="about">
+  <div class="w3-content" >
+    <h5 class="w3-center w3-padding-75"><span class="w3-tag w3-wide">ABOUT SOFFICE</span></h5>
+    <p>The SOFFICE was founded in blabla by Mr.  in lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p>In addition to our Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla eveniet facere sed temporibus nobis doloremque, eius voluptatem illo laborum quam! Aliquam tempora corrupti repellat expedita quibusdam sit voluptates quod alias.</p>
+     </div>
+     </div>
+     </div>
+     </div>
+     )
+}
+}
+export default Shome;
